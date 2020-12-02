6dc0dcdbded7ba0ea52ab200d72e75fe2bd1d962
christianwaldmann <waldmann-christian@web.de>
2020-12-02 15:28:00 +0100

    Stand Video07_Premake, Add and setup premake

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Orange"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist",
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "Orange"
+	location "Orange"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .."/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .."/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"OG_PLATFORM_WINDOWS",
+			"OG_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "OG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OG_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .."/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .."/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Orange/vendor/spdlog/include",
+		"Orange/src",
+	}
+
+	links
+	{
+		"Orange"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines
+		{
+			"OG_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "OG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OG_DIST"
+		optimize "On"
