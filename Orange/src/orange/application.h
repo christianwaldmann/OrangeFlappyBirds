3e4a31f0ed753c16bec0381e20f4dcaf67331c36
christianwaldmann <waldmann-christian@web.de>
2020-12-08 19:02:24 +0100

    Stand Video32_VertexArrays, Add vertex array and render blue square

--- Orange/src/orange/application.h
+++ Orange/src/orange/application.h
@@ -10,6 +10,7 @@
 
 #include "orange/Renderer/Shader.h"
 #include "orange/Renderer/Buffer.h"
+#include "orange/Renderer/VertexArray.h"
 
 
 namespace Orange {
@@ -38,10 +39,11 @@ namespace Orange {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 
 	private:
 		static application* s_Instance;
