6474e1e1cd0b543e1c83d6709bb02180bd1d2ef9
christianwaldmann <waldmann-christian@web.de>
2020-12-13 17:44:07 +0100

    Fix TODO: Add functions for uploading uniforms to shader class

--- Orange/src/Orange/Renderer/Renderer2D.cpp
+++ Orange/src/Orange/Renderer/Renderer2D.cpp
@@ -6,9 +6,6 @@
 
 #include "RenderCommand.h"
 
-// TEMP
-#include "Platform/OpenGL/OpenGLShader.h"
-
 
 namespace Orange {
 
@@ -57,9 +54,9 @@ namespace Orange {
 
 
 	void Renderer2D::BeginScene(const OrthographicCamera& camera) {
-		std::dynamic_pointer_cast<OpenGLShader>(s_Data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<OpenGLShader>(s_Data->FlatColorShader)->UploadUniformMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-		std::dynamic_pointer_cast<OpenGLShader>(s_Data->FlatColorShader)->UploadUniformMat4("u_Transform", glm::mat4(1.0f));
+		s_Data->FlatColorShader->Bind();
+		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
+		s_Data->FlatColorShader->SetMat4("u_Transform", glm::mat4(1.0f));
 	}
 
 
@@ -74,8 +71,8 @@ namespace Orange {
 
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color) {
-		std::dynamic_pointer_cast<OpenGLShader>(s_Data->FlatColorShader)->Bind();
-		std::dynamic_pointer_cast<Orange::OpenGLShader>(s_Data->FlatColorShader)->UploadUniformFloat4("u_Color", color);
+		s_Data->FlatColorShader->Bind();
+		s_Data->FlatColorShader->SetFloat4("u_Color", color);
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -5,9 +5,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-// TEMP
-#include "Platform/OpenGL/OpenGLShader.h"
-
 
 Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f) {}
 
@@ -35,10 +32,6 @@ void Sandbox2D::OnUpdate(Orange::Timestep ts) {
 	Orange::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 
 	Orange::Renderer2D::EndScene();
-
-	//// TODO: Add these functions - Shader::SetMat4(), Shader::SetFloat4()
-	//std::dynamic_pointer_cast<Orange::OpenGLShader>(m_FlatColorShader)->Bind();
-	//std::dynamic_pointer_cast<Orange::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 
