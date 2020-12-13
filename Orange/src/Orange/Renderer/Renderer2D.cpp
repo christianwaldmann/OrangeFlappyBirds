52f201666c9f618d9ebd8a54e2573af9730005fe
christianwaldmann <waldmann-christian@web.de>
2020-12-13 17:59:33 +0100

    Stand Video52_2DRendererTransforms, Add transforms to 2D renderer

--- Orange/src/Orange/Renderer/Renderer2D.cpp
+++ Orange/src/Orange/Renderer/Renderer2D.cpp
@@ -6,6 +6,8 @@
 
 #include "RenderCommand.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 
 namespace Orange {
 
@@ -56,7 +58,6 @@ namespace Orange {
 	void Renderer2D::BeginScene(const OrthographicCamera& camera) {
 		s_Data->FlatColorShader->Bind();
 		s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-		s_Data->FlatColorShader->SetMat4("u_Transform", glm::mat4(1.0f));
 	}
 
 
@@ -73,6 +74,9 @@ namespace Orange {
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color) {
 		s_Data->FlatColorShader->Bind();
 		s_Data->FlatColorShader->SetFloat4("u_Color", color);
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		s_Data->FlatColorShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
 		RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
