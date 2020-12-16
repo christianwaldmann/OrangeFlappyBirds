628b8e19f90a7ceb61046bfa4cc582ee46700707
christianwaldmann <waldmann-christian@web.de>
2020-12-16 17:54:57 +0100

    Add functions for drawing rotated quads

--- Orange/src/Orange/Renderer/Renderer2D.h
+++ Orange/src/Orange/Renderer/Renderer2D.h
@@ -21,6 +21,11 @@ namespace Orange {
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
 		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f);
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor = 1.0f);
+
+		static void DrawQuadRotated(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+		static void DrawQuadRotated(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color);
+		static void DrawQuadRotated(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f);
+		static void DrawQuadRotated(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor = 1.0f);
 	};
 
 }
